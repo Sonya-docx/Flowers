29a7c2d16e22d073bdd561db820099a2302c0a24
Sonya-docx <sonyaivanchenko1@gmail.com>
2025-12-13 00:42:10 +0200

    api commit

--- catalog_api.js
+++ catalog_api.js
@@ -1,6 +1,6 @@
 async function loadBouquets() {
     try {
-        const response = await fetch("https://api.jsonbin.io/v3/b/693c9619ae596e708f95ea86"); 
+        const response = await fetch("https://api.jsonbin.io/v3/b/693c9619ae596e708f95ea86/latest"); 
         const data = await response.json();
         renderCatalog(data.record);
     } catch (error) {
