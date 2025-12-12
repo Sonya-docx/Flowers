28608e6fb6862510a484fcc0c1363c4235ca8f6f
Sonya-docx <sonyaivanchenko1@gmail.com>
2025-12-13 00:45:47 +0200

    api

--- catalog_api.js
+++ catalog_api.js
@@ -1,8 +1,16 @@
 async function loadBouquets() {
     try {
-        const response = await fetch("https://api.jsonbin.io/v3/b/693c9619ae596e708f95ea86/latest"); 
+        const response = await fetch(
+            "https://api.jsonbin.io/v3/b/693c9619ae596e708f95ea86/latest"
+        );
+
         const data = await response.json();
+
+        console.log("Отримано:", data);
+
+        // ДУЖЕ ВАЖЛИВО → у JSONBin дані лежать у data.record
         renderCatalog(data.record);
+
     } catch (error) {
         console.error("Помилка завантаження:", error);
     }
