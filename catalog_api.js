d86cb030405fee98608693fb5c63d43dd38cab36
Sonya-docx <sonyaivanchenko1@gmail.com>
2025-12-13 00:34:45 +0200

    api commit

--- catalog_api.js
+++ catalog_api.js
@@ -1,8 +1,8 @@
 async function loadBouquets() {
     try {
-        const response = await fetch("https://my-json-server.typicode.com/Sonya-docx/Flowers/db.json"); 
+        const response = await fetch("https://api.jsonbin.io/v3/b/693c9619ae596e708f95ea86"); 
         const data = await response.json();
-        renderCatalog(data);
+        renderCatalog(data.record);
     } catch (error) {
         console.error("Помилка завантаження:", error);
     }
