bf28b3ce510f783a4e609d36207d162b371e26f5
Sonya-docx <sonyaivanchenko1@gmail.com>
2025-12-12 21:27:41 +0200

    c

--- catalog_api.js
+++ catalog_api.js
@@ -1,6 +1,6 @@
 async function loadBouquets() {
     try {
-        const response = await fetch("./server/data.json"); 
+        const response = await fetch("data.json"); 
         const data = await response.json();
         renderCatalog(data);
     } catch (error) {
